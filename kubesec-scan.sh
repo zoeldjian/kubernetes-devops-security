50a0844422579cc60caf4fc70c65a2f6339344c9
Ubuntu <ubuntu@ip-172-31-0-143.ap-southeast-1.compute.internal>
2022-02-20 06:43:54 +0000

    Added rules on deployment.yaml

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -17,7 +17,7 @@
     # Kubesec scan result processing
     # echo "Scan Score : $scan_score"
 
-	if [[ "${scan_score}" -ge 4 ]]; then
+	if [[ "${scan_score}" -ge 3 ]]; then
 	    echo "Score is $scan_score"
 	    echo "Kubesec Scan $scan_message"
 	else
